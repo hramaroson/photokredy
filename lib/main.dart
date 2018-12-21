82d91c3d903073f3723608f7d90c9250191a627e
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2018-12-22 00:49:05 +0300

    Add Shared Preferences capability

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
+import 'package:shared_preferences/shared_preferences.dart';
+void main() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
