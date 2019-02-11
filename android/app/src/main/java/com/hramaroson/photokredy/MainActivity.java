83d70ce7fe0fafaab97f5cef3af7b076f93a6e67
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2019-02-11 22:22:29 +0300

    Lock Activity orientation to Portrait mode (Android)

--- android/app/src/main/java/com/hramaroson/photokredy/MainActivity.java
+++ android/app/src/main/java/com/hramaroson/photokredy/MainActivity.java
@@ -3,11 +3,14 @@ package com.hramaroson.photokredy;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.content.pm.ActivityInfo;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
   }
 }
